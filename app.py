8bf3c160102356ba766dbd2b0dfef94ccbf265a9
Agustin <agustincepeda1@gmail.com>
2022-11-17 14:46:54 -0300

    proyecto casi terminado

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
+import os
 
 app = Flask(__name__)
 
+picFolder=os.path.join('static','pics')
+
+app.config['UPLOAD_FOLDER'] =picFolder
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    pic1 = os.path.join(app.config['UPLOAD_FOLDER'],'servicio.jpg')
+    return render_template("index.html",user_image=pic1)
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
